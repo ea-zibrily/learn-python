ee3efcc806d0312634c1c7742d91e3d9a4f1b0c8
Zibrrr <zyuuannn@gmail.com>
2022-11-12 21:25:59 +0700

    yok gas

---
+++ Learn 1/phy1.py
@@ -0,0 +1,9 @@
+#coba program python 1
+#by ea-zbryly
+
+afaMarbles = 260
+print("Jumlah kelereng awal Afa adalah %d" %afaMarbles)
+
+loseMarbles = afaMarbles - 180
+
+print("Jumlah kelereng Afa setelah kalah adalah %d" %loseMarbles)
---
+++ Learn 1/phy2.py
@@ -0,0 +1,16 @@
+eraserCost = 1800
+dozenEraser = 1800 * 12
+luthfanMoney = 10000 * 3
+totalCost = luthfanMoney - dozenEraser
+
+print("Eraser Cost Program")
+print("--------------------")
+
+print("Luthfan ingin membeli satu lusin penghapus")
+
+print("")
+
+print("Harga sebuah penghapus: %d" %eraserCost)
+print("Harga satu lusin penghapus: %d" %dozenEraser)
+print("Uang Luthfan: %d" %luthfanMoney)
+print("Total kembalian Lutfhan: %d" %totalCost)
